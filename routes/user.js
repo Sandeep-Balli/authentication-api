bd0ca756f9ee10a2a4449edcbdeb2564d9a7d017
Sandeep-Balli <sandeepballi.tech@gmail.com>
2022-10-08 00:18:50 +0530

    Screenshots Added

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,7 @@ router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
         const {password, ...others} = user._doc;
-        res.status(500).json(others);
+        res.status(200).json(others);
     } catch(err) {
         res.status(500).json(err);
     }
